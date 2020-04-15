0296bf4e30f45569fa0a2ed4937aef0954546271
Dushan Dissanayake <42910542+dushanDissanayake@users.noreply.github.com>
2020-04-15 15:55:05 +0530

    Updates

---
+++ precache-manifest.eeb2d04c2cc1a03ffea7bbb90a67dff5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d33c9c5d58577c56a8f7e0f29d9b530c",
+    "url": "/React-Social/index.html"
+  },
+  {
+    "revision": "cf474f9c7606c45d0202",
+    "url": "/React-Social/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "75a19d17800a15226ce6",
+    "url": "/React-Social/static/js/2.67da2b16.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Social/static/js/2.67da2b16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf474f9c7606c45d0202",
+    "url": "/React-Social/static/js/main.8084aa72.chunk.js"
+  },
+  {
+    "revision": "c61fb039485475120c90",
+    "url": "/React-Social/static/js/runtime-main.f52a9c7c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-Social/static/media/logo.5d5d9eef.svg"
+  }
+]);
